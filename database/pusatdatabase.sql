576b9a21b1b4f847fec2dbe81e2948b76f9ca5b7
msimam <msimam143@gmail.com>
2022-10-29 17:23:39 +0700

    chanel

---
+++ database/pusatdatabase.sql
@@ -0,0 +1,251 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.11.0-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for proyek
+CREATE DATABASE IF NOT EXISTS `proyek` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `proyek`;
+
+-- Dumping structure for table proyek.about_pribadi
+CREATE TABLE IF NOT EXISTS `about_pribadi` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.about_pribadi: ~0 rows (approximately)
+/*!40000 ALTER TABLE `about_pribadi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `about_pribadi` ENABLE KEYS */;
+
+-- Dumping structure for table proyek.auth_group
+CREATE TABLE IF NOT EXISTS `auth_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.auth_group: ~0 rows (approximately)
+/*!40000 ALTER TABLE `auth_group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_group` ENABLE KEYS */;
+
+-- Dumping structure for table proyek.auth_group_permissions
+CREATE TABLE IF NOT EXISTS `auth_group_permissions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
+  KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.auth_group_permissions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `auth_group_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_group_permissions` ENABLE KEYS */;
+
+-- Dumping structure for table proyek.auth_permission
+CREATE TABLE IF NOT EXISTS `auth_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  `codename` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
+  CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.auth_permission: ~28 rows (approximately)
+/*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
+INSERT INTO `auth_permission` (`id`, `name`, `content_type_id`, `codename`) VALUES
+	(1, 'Can add log entry', 1, 'add_logentry'),
+	(2, 'Can change log entry', 1, 'change_logentry'),
+	(3, 'Can delete log entry', 1, 'delete_logentry'),
+	(4, 'Can view log entry', 1, 'view_logentry'),
+	(5, 'Can add permission', 2, 'add_permission'),
+	(6, 'Can change permission', 2, 'change_permission'),
+	(7, 'Can delete permission', 2, 'delete_permission'),
+	(8, 'Can view permission', 2, 'view_permission'),
+	(9, 'Can add group', 3, 'add_group'),
+	(10, 'Can change group', 3, 'change_group'),
+	(11, 'Can delete group', 3, 'delete_group'),
+	(12, 'Can view group', 3, 'view_group'),
+	(13, 'Can add user', 4, 'add_user'),
+	(14, 'Can change user', 4, 'change_user'),
+	(15, 'Can delete user', 4, 'delete_user'),
+	(16, 'Can view user', 4, 'view_user'),
+	(17, 'Can add content type', 5, 'add_contenttype'),
+	(18, 'Can change content type', 5, 'change_contenttype'),
+	(19, 'Can delete content type', 5, 'delete_contenttype'),
+	(20, 'Can view content type', 5, 'view_contenttype'),
+	(21, 'Can add session', 6, 'add_session'),
+	(22, 'Can change session', 6, 'change_session'),
+	(23, 'Can delete session', 6, 'delete_session'),
+	(24, 'Can view session', 6, 'view_session'),
+	(25, 'Can add pribadi', 7, 'add_pribadi'),
+	(26, 'Can change pribadi', 7, 'change_pribadi'),
+	(27, 'Can delete pribadi', 7, 'delete_pribadi'),
+	(28, 'Can view pribadi', 7, 'view_pribadi');
+/*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
+
+-- Dumping structure for table proyek.auth_user
+CREATE TABLE IF NOT EXISTS `auth_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.auth_user: ~1 rows (approximately)
+/*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
+INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES
+	(1, 'pbkdf2_sha256$390000$dbcUnIbZr2eq4Ek0SMGvVW$qchKbypw5OwOP24N0Bv0yrkglt1vtACKSVs8WKX3JdE=', '2022-10-29 08:23:18.834722', 1, 'admin', '', '', 'imam@gmail.com', 1, 1, '2022-10-29 05:10:04.228034');
+/*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
+
+-- Dumping structure for table proyek.auth_user_groups
+CREATE TABLE IF NOT EXISTS `auth_user_groups` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_groups_user_id_group_id_94350c0c_uniq` (`user_id`,`group_id`),
+  KEY `auth_user_groups_group_id_97559544_fk_auth_group_id` (`group_id`),
+  CONSTRAINT `auth_user_groups_group_id_97559544_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.auth_user_groups: ~0 rows (approximately)
+/*!40000 ALTER TABLE `auth_user_groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_user_groups` ENABLE KEYS */;
+
+-- Dumping structure for table proyek.auth_user_user_permissions
+CREATE TABLE IF NOT EXISTS `auth_user_user_permissions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`),
+  KEY `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.auth_user_user_permissions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `auth_user_user_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_user_user_permissions` ENABLE KEYS */;
+
+-- Dumping structure for table proyek.django_admin_log
+CREATE TABLE IF NOT EXISTS `django_admin_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `action_time` datetime(6) NOT NULL,
+  `object_id` longtext DEFAULT NULL,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint(5) unsigned NOT NULL CHECK (`action_flag` >= 0),
+  `change_message` longtext NOT NULL,
+  `content_type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`),
+  KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`),
+  CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.django_admin_log: ~0 rows (approximately)
+/*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
+
+-- Dumping structure for table proyek.django_content_type
+CREATE TABLE IF NOT EXISTS `django_content_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.django_content_type: ~7 rows (approximately)
+/*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
+INSERT INTO `django_content_type` (`id`, `app_label`, `model`) VALUES
+	(7, 'about', 'pribadi'),
+	(1, 'admin', 'logentry'),
+	(3, 'auth', 'group'),
+	(2, 'auth', 'permission'),
+	(4, 'auth', 'user'),
+	(5, 'contenttypes', 'contenttype'),
+	(6, 'sessions', 'session');
+/*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
+
+-- Dumping structure for table proyek.django_migrations
+CREATE TABLE IF NOT EXISTS `django_migrations` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.django_migrations: ~19 rows (approximately)
+/*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
+INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
+	(1, 'contenttypes', '0001_initial', '2022-10-29 04:58:14.871553'),
+	(2, 'auth', '0001_initial', '2022-10-29 04:58:15.757417'),
+	(3, 'admin', '0001_initial', '2022-10-29 04:58:15.949149'),
+	(4, 'admin', '0002_logentry_remove_auto_add', '2022-10-29 04:58:15.975969'),
+	(5, 'admin', '0003_logentry_add_action_flag_choices', '2022-10-29 04:58:15.992605'),
+	(6, 'contenttypes', '0002_remove_content_type_name', '2022-10-29 04:58:16.150033'),
+	(7, 'auth', '0002_alter_permission_name_max_length', '2022-10-29 04:58:16.209043'),
+	(8, 'auth', '0003_alter_user_email_max_length', '2022-10-29 04:58:16.275675'),
+	(9, 'auth', '0004_alter_user_username_opts', '2022-10-29 04:58:16.292034'),
+	(10, 'auth', '0005_alter_user_last_login_null', '2022-10-29 04:58:16.386126'),
+	(11, 'auth', '0006_require_contenttypes_0002', '2022-10-29 04:58:16.394632'),
+	(12, 'auth', '0007_alter_validators_add_error_messages', '2022-10-29 04:58:16.411323'),
+	(13, 'auth', '0008_alter_user_username_max_length', '2022-10-29 04:58:16.475640'),
+	(14, 'auth', '0009_alter_user_last_name_max_length', '2022-10-29 04:58:16.691294'),
+	(15, 'auth', '0010_alter_group_name_max_length', '2022-10-29 04:58:16.734924'),
+	(16, 'auth', '0011_update_proxy_permissions', '2022-10-29 04:58:16.754421'),
+	(17, 'auth', '0012_alter_user_first_name_max_length', '2022-10-29 04:58:16.794692'),
+	(18, 'sessions', '0001_initial', '2022-10-29 04:58:16.916271'),
+	(19, 'about', '0001_initial', '2022-10-29 08:34:27.410441');
+/*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
+
+-- Dumping structure for table proyek.django_session
+CREATE TABLE IF NOT EXISTS `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime(6) NOT NULL,
+  PRIMARY KEY (`session_key`),
+  KEY `django_session_expire_date_a5c62663` (`expire_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table proyek.django_session: ~1 rows (approximately)
+/*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
+INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALUES
+	('5xm8y1w3tpv99e7f43iyy7l8dlf3ocwu', '.eJxVjDsOwyAQRO9CHSHM16RM7zOgXViCkwgkY1dR7h5bcpFounlv5s0CbGsJW6clzIld2cAuvx1CfFI9QHpAvTceW12XGfmh8JN2PrVEr9vp_h0U6GVfk0YXlR4hQxKQ0XqrPAqUeSC3RxsnLZLIkdB4VETo7ZiklCqDE4Z9vg5FOMY:1ooh7e:MqJ7M3jhzMWY1UGtUXyXEbznp4_eRCJJ_36ooTVdrss', '2022-11-12 08:23:18.838285');
+/*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
